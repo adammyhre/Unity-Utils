6ca2106890bc26a6ffa75b8ef953e09378ff53d2
Adam Myhre <adam@undergalaxie.com>
2023-11-20 16:54:05 -0800

    Update Vector3 Extensions

--- Assets/_Project/Scripts/Extensions/Vector3Extensions.cs
+++ Assets/_Project/Scripts/Extensions/Vector3Extensions.cs
@@ -7,12 +7,12 @@ public static class Vector3Extensions {
     public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null) {
         return new Vector3(x ?? vector.x, y ?? vector.y, z ?? vector.z);
     }
-    
+
     /// <summary>
     /// Adds to any x y z values of a Vector3
     /// </summary>
-    public static Vector3 Add(this Vector3 vector, float? x = null, float? y = null, float? z = null) {
-        return new Vector3(vector.x + (x ?? 0), vector.y + (y ?? 0), vector.z + (z ?? 0));
+    public static Vector3 Add(this Vector3 vector, float x = 0, float y = 0, float z = 0) {
+        return new Vector3(vector.x + x, vector.y + y, vector.z + z);
     }
 
     /// <summary>
