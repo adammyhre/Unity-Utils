eb7484c6296840443d2d36205b7b91cc77cf835d
adammyhre <38876398+adammyhre@users.noreply.github.com>
2023-12-09 20:02:31 -0800

    Add Resources Extension class

---
+++ Assets/_Project/Scripts/Extensions/ResourcesUtils.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public static class ResourcesUtils {
+    public static void LoadVolumeProfile(this Volume volume, string path) {
+        var profile = Resources.Load<VolumeProfile>(path);
+        volume.profile = profile;
+    }
+}
