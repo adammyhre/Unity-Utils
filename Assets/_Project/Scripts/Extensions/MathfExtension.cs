c54877f0c2b5ee182c6439e10b7e596c05c9884f
Andrzej Kebab <grzesiugora123@wp.pl>
2023-12-31 21:31:38 +0100

    rewriting docs

--- Assets/_Project/Scripts/Extensions/MathfExtension.cs
+++ Assets/_Project/Scripts/Extensions/MathfExtension.cs
@@ -2,234 +2,203 @@
 using Unity.Mathematics;
 #endif
 
+/// <summary>
+/// A utility struct providing extension methods for Mathf.
+/// </summary>
 public struct MathfExtension
 {
 	#region Min
-		/// <summary>
-		/// Returns the smallest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static long Min(long a, long b)
+	/// <summary>
+	/// Returns the smaller of two long values.
+	/// </summary>
+	public static long Min(long a, long b)
+	{
+		return (a < b) ? a : b;
+	}
+
+	/// <summary>
+	/// Returns the smallest value among an array of long values.
+	/// </summary>
+	public static long Min(params long[] values)
+	{
+		int num = values.Length;
+		if (num == 0)
 		{
-			return (a < b) ? a : b;
+			return 0;
 		}
 
-		/// <summary>
-		/// Returns the smallest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static long Min(params long[] values)
+		long num2 = values[0];
+		for (int i = 1; i < num; i++)
 		{
-			int num = values.Length;
-			if (num == 0)
+			if (values[i] < num2)
 			{
-				return 0;
+				num2 = values[i];
 			}
-
-			long num2 = values[0];
-			for (int i = 1; i < num; i++)
-			{
-				if (values[i] < num2)
-				{
-					num2 = values[i];
-				}
-			}
-
-			return num2;
 		}
 
-	#if ENABLED_UNITY_MATHEMATICS
-		/// <summary>
-		/// Returns the smallest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static half Min(half a, half b)
+		return num2;
+	}
+
+#if ENABLED_UNITY_MATHEMATICS
+	/// <summary>
+	/// Returns the smaller of two half-precision floating-point values.
+	/// </summary>
+	public static half Min(half a, half b)
+	{
+		return (a < b) ? a : b;
+	}
+
+	/// <summary>
+	/// Returns the smallest value among an array of half-precision floating-point values.
+	/// </summary>
+	public static half Min(params half[] values)
+	{
+		int num = values.Length;
+		if (num == 0)
 		{
-			return (a < b) ? a : b;
+			return (half)0;
 		}
 
-		/// <summary>
-		/// Returns the smallest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static half Min(params half[] values)
+		half num2 = values[0];
+		for (int i = 1; i < num; i++)
 		{
-			int num = values.Length;
-			if (num == 0)
+			if (values[i] < num2)
 			{
-				return (half)0;
+				num2 = values[i];
 			}
-
-			half num2 = values[0];
-			for (int i = 1; i < num; i++)
-			{
-				if (values[i] < num2)
-				{
-					num2 = values[i];
-				}
-			}
-
-			return num2;
-		}
-	#endif
-
-		/// <summary>
-		/// Returns the smallest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static double Min(double a, double b)
-		{
-			return (a < b) ? a : b;
 		}
 
-		/// <summary>
-		/// Returns the smallest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static double Min(params double[] values)
+		return num2;
+	}
+#endif
+
+	/// <summary>
+	/// Returns the smaller of two double-precision floating-point values.
+	/// </summary>
+	public static double Min(double a, double b)
+	{
+		return (a < b) ? a : b;
+	}
+
+	/// <summary>
+	/// Returns the smallest value among an array of double-precision floating-point values.
+	/// </summary>
+	public static double Min(params double[] values)
+	{
+		int num = values.Length;
+		if (num == 0)
 		{
-			int num = values.Length;
-			if (num == 0)
-			{
-				return 0f;
-			}
-
-			double num2 = values[0];
-			for (int i = 1; i < num; i++)
-			{
-				if (values[i] < num2)
-				{
-					num2 = values[i];
-				}
-			}
-
-			return num2;
+			return 0f;
 		}
+
+		double num2 = values[0];
+		for (int i = 1; i < num; i++)
+		{
+			if (values[i] < num2)
+			{
+				num2 = values[i];
+			}
+		}
+
+		return num2;
+	}
 	#endregion
+
 	#region Max
-		/// <summary>
-		/// Returns the largest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static long Max(long a, long b)
+	/// <summary>
+	/// Returns the larger of two long values.
+	/// </summary>
+	public static long Max(long a, long b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	/// <summary>
+	/// Returns the largest value among an array of long values.
+	/// </summary>
+	public static long Max(params long[] values)
+	{
+		int num = values.Length;
+		if (num == 0)
 		{
-			return (a > b) ? a : b;
+			return 0;
 		}
 
-		/// <summary>
-		/// Returns the largest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static long Max(params long[] values)
+		long num2 = values[0];
+		for (int i = 1; i < num; i++)
 		{
-			int num = values.Length;
-			if (num == 0)
+			if (values[i] > num2)
 			{
-				return 0;
+				num2 = values[i];
 			}
-
-			long num2 = values[0];
-			for (int i = 1; i < num; i++)
-			{
-				if (values[i] > num2)
-				{
-					num2 = values[i];
-				}
-			}
-
-			return num2;
 		}
 
-	#if ENABLED_UNITY_MATHEMATICS
-		/// <summary>
-		/// Returns the largest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static half Max(half a, half b)
+		return num2;
+	}
+
+#if ENABLED_UNITY_MATHEMATICS
+	/// <summary>
+	/// Returns the larger of two half-precision floating-point values.
+	/// </summary>
+	public static half Max(half a, half b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	/// <summary>
+	/// Returns the largest value among an array of half-precision floating-point values.
+	/// </summary>
+	public static half Max(params half[] values)
+	{
+		int num = values.Length;
+		if (num == 0)
 		{
-			return (a > b) ? a : b;
+			return (half)0;
 		}
 
-		/// <summary>
-		/// Returns the largest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static half Max(params half[] values)
+		half num2 = values[0];
+		for (int i = 1; i < num; i++)
 		{
-			int num = values.Length;
-			if (num == 0)
+			if (values[i] > num2)
 			{
-				return (half)0;
+				num2 = values[i];
 			}
-
-			half num2 = values[0];
-			for (int i = 1; i < num; i++)
-			{
-				if (values[i] > num2)
-				{
-					num2 = values[i];
-				}
-			}
-
-			return num2;
-		}
-	#endif
-
-		/// <summary>
-		/// Returns the largest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static double Max(double a, double b)
-		{
-				return (a > b) ? a : b;
 		}
 
-		/// <summary>
-		/// Returns the largest of two or more values.
-		/// </summary>
-		/// <param name="a"></param>
-		/// <param name="b"></param>
-		/// <returns></returns>
-		public static double Max(params double[] values)
+		return num2;
+	}
+#endif
+
+	/// <summary>
+	/// Returns the larger of two double-precision floating-point values.
+	/// </summary>
+	public static double Max(double a, double b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	/// <summary>
+	/// Returns the largest value among an array of double-precision floating-point values.
+	/// </summary>
+	public static double Max(params double[] values)
+	{
+		int num = values.Length;
+		if (num == 0)
 		{
-			int num = values.Length;
-			if (num == 0)
-			{
-				return 0f;
-			}
-
-			double num2 = values[0];
-			for (int i = 1; i < num; i++)
-			{
-				if (values[i] > num2)
-				{
-					num2 = values[i];
-				}
-			}
-
-			return num2;
+			return 0f;
 		}
+
+		double num2 = values[0];
+		for (int i = 1; i < num; i++)
+		{
+			if (values[i] > num2)
+			{
+				num2 = values[i];
+			}
+		}
+
+		return num2;
+	}
+
 	#endregion
 }
