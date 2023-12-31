1421d8f74a3bc093fbf61b03653e573e4909d579
Andrzej Kebab <grzesiugora123@wp.pl>
2023-12-31 20:46:43 +0100

    More NumberExtensions and Vector2 typo Fix

---
+++ Assets/_Project/Scripts/Extensions/MathfExtension.cs
@@ -0,0 +1,217 @@
+﻿using Unity.Mathematics;
+
+public struct MathfExtension
+{
+	#region Min
+		/// <summary>
+		/// Returns the smallest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static long Min(long a, long b)
+		{
+			return (a < b) ? a : b;
+		}
+
+		/// <summary>
+		/// Returns the smallest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static long Min(params long[] values)
+		{
+			int num = values.Length;
+			if (num == 0)
+			{
+				return 0;
+			}
+
+			long num2 = values[0];
+			for (int i = 1; i < num; i++)
+			{
+				if (values[i] < num2)
+				{
+					num2 = values[i];
+				}
+			}
+
+			return num2;
+		}
+
+		/// <summary>
+		/// Returns the smallest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static half Min(half a, half b)
+		{
+			return (a < b) ? a : b;
+		}
+
+		/// <summary>
+		/// Returns the smallest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static half Min(params half[] values)
+		{
+			int num = values.Length;
+			if (num == 0)
+			{
+				return (half)0;
+			}
+
+			half num2 = values[0];
+			for (int i = 1; i < num; i++)
+			{
+				if (values[i] < num2)
+				{
+					num2 = values[i];
+				}
+			}
+
+			return num2;
+		}
+
+		/// <summary>
+		/// Returns the smallest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double Min(double a, double b)
+		{
+			return (a < b) ? a : b;
+		}
+
+		/// <summary>
+		/// Returns the smallest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double Min(params double[] values)
+		{
+			int num = values.Length;
+			if (num == 0)
+			{
+				return 0f;
+			}
+
+			double num2 = values[0];
+			for (int i = 1; i < num; i++)
+			{
+				if (values[i] < num2)
+				{
+					num2 = values[i];
+				}
+			}
+
+			return num2;
+		}
+	#endregion
+	#region Max
+	public static long Max(long a, long b)
+		{
+			return (a > b) ? a : b;
+		}
+
+		public static long Max(params long[] values)
+		{
+			int num = values.Length;
+			if (num == 0)
+			{
+				return 0;
+			}
+
+			long num2 = values[0];
+			for (int i = 1; i < num; i++)
+			{
+				if (values[i] > num2)
+				{
+					num2 = values[i];
+				}
+			}
+
+			return num2;
+		}
+
+		/// <summary>
+		/// Returns the largest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static half Max(half a, half b)
+		{
+			return (a > b) ? a : b;
+		}
+
+		/// <summary>
+		/// Returns the largest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static half Max(params half[] values)
+		{
+			int num = values.Length;
+			if (num == 0)
+			{
+				return (half)0;
+			}
+
+			half num2 = values[0];
+			for (int i = 1; i < num; i++)
+			{
+				if (values[i] > num2)
+				{
+					num2 = values[i];
+				}
+			}
+
+			return num2;
+		}
+
+		/// <summary>
+		/// Returns the largest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double Max(double a, double b)
+		{
+				return (a > b) ? a : b;
+		}
+
+		/// <summary>
+		/// Returns the largest of two or more values.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public static double Max(params double[] values)
+		{
+			int num = values.Length;
+			if (num == 0)
+			{
+				return 0f;
+			}
+
+			double num2 = values[0];
+			for (int i = 1; i < num; i++)
+			{
+				if (values[i] > num2)
+				{
+					num2 = values[i];
+				}
+			}
+
+			return num2;
+		}
+	#endregion
+}
--- Assets/_Project/Scripts/Extensions/NumberExtensions.cs
+++ Assets/_Project/Scripts/Extensions/NumberExtensions.cs
@@ -1,14 +1,43 @@
 ﻿using UnityEngine;
+using MathfExt = MathfExtension;
+#if ENABLED_UNITY_MATHEMATICS
+using Unity.Mathematics;
+#endif
 
-public static class NumberExtensions {
-    public static float PercentageOf(this int part, int whole) {
-        if (whole == 0) return 0; // Handling division by zero
-        return (float) part / whole;
-    }
-    
-    public static int AtLeast(this int value, int min) => Mathf.Max(value, min);
-    public static int AtMost(this int value, int max) => Mathf.Min(value, max);
+public static class NumberExtensions
+{
+	public static float PercentageOf(this float part, float whole)
+	{
+		if (whole == 0) return 0;
+		return (float)part / whole;
+	}
 
-    public static float AtLeast(this float value, float min) => Mathf.Max(value, min);
-    public static float AtMost(this float value, float max) => Mathf.Min(value, max);
+	public static double PercentageOf(this double part, double whole)
+	{
+		if (whole == 0) return 0;
+		return (double)part / whole;
+	}
+
+	public static byte AtLeast(this byte value, short min) => (byte)Mathf.Max(value, min);
+	public static byte AtMost(this byte value, short min) => (byte)Mathf.Min(value, min);
+
+	public static short AtLeast(this short value, short min) => (short)Mathf.Max(value, min);
+	public static short AtMost(this short value, short min) => (short)Mathf.Min(value, min);
+
+	public static int AtLeast(this int value, int min) => Mathf.Max(value, min);
+	public static int AtMost(this int value, int max) => Mathf.Min(value, max);
+
+	public static long AtLeast(this long value, long max) => MathfExt.Max(value, max);
+	public static long AtMost(this long value, long max) => MathfExt.Min(value, max);
+
+#if ENABLED_UNITY_MATHEMATICS
+	public static half AtLeast(this half value, half max) => MathfExt.Max(value, max);
+	public static half AtMost(this half value, half max) => MathfExt.Min(value, max);
+#endif
+
+	public static float AtLeast(this float value, float min) => Mathf.Max(value, min);
+	public static float AtMost(this float value, float max) => Mathf.Min(value, max);
+
+	public static double AtLeast(this double value, double min) => MathfExt.Max(value, min);
+	public static double AtMost(this double value, double min) => MathfExt.Min(value, min);
 }
