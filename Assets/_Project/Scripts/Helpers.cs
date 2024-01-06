4327c422f47fc7f80f2c1fadeb8f6ed61ed4c374
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-01-06 06:45:13 -0800

    Add Helpers Class For Static Helper Methods
    Includes a Dictionary and Static Method for retrieving WaitForSeconds

---
+++ Assets/_Project/Scripts/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helpers {
+    static readonly Dictionary<float, WaitForSeconds> WaitForSeconds = new();
+
+    /// <summary>
+    /// Returns a WaitForSeconds object for the specified duration. </summary>
+    /// <param name="seconds">The duration in seconds to wait.</param>
+    /// <returns>A WaitForSeconds object.</returns>
+    public static WaitForSeconds GetWaitForSeconds(float seconds) {
+        if (WaitForSeconds.TryGetValue(seconds, out var forSeconds)) return forSeconds;
+
+        var waitForSeconds = new WaitForSeconds(seconds);
+        WaitForSeconds.Add(seconds, waitForSeconds);
+        return WaitForSeconds[seconds];
+    }
+}
