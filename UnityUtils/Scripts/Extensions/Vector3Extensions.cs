e90db683dd1bfab8bd17deee272230aa28635537
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-01-28 17:24:17 -0800

    Add Vector3.ComponentDivide
    Adds extension method to proportionally scale a Vector3

--- UnityUtils/Scripts/Extensions/Vector3Extensions.cs
+++ UnityUtils/Scripts/Extensions/Vector3Extensions.cs
@@ -26,5 +26,29 @@ namespace UnityUtils {
         public static bool InRangeOf(this Vector3 current, Vector3 target, float range) {
             return (current - target).sqrMagnitude <= range * range;
         }
+        
+        /// <summary>
+        /// Divides two Vector3 objects component-wise.
+        /// </summary>
+        /// <remarks>
+        /// For each component in v0 (x, y, z), it is divided by the corresponding component in v1 if the component in v1 is not zero. 
+        /// Otherwise, the component in v0 remains unchanged.
+        /// </remarks>
+        /// <example>
+        /// Use 'ComponentDivide' to scale a game object proportionally:
+        /// <code>
+        /// myObject.transform.localScale = originalScale.ComponentDivide(targetDimensions);
+        /// </code>
+        /// This scales the object size to fit within the target dimensions while maintaining its original proportions.
+        ///</example>
+        /// <param name="v0">The Vector3 object that this method extends.</param>
+        /// <param name="v1">The Vector3 object by which v0 is divided.</param>
+        /// <returns>A new Vector3 object resulting from the component-wise division.</returns>
+        public static Vector3 ComponentDivide(this Vector3 v0, Vector3 v1){
+            return new Vector3( 
+                v1.x != 0 ? v0.x / v1.x : v0.x, 
+                v1.y != 0 ? v0.y / v1.y : v0.y, 
+                v1.z != 0 ? v0.z / v1.z : v0.z);  
+        }        
     }
 }
