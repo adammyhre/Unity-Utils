e1049ceba83b6bf3cb3f5512910e7fafe9e18d69
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-03-17 19:24:32 -0700

    Add LayerMask Extensions Class

---
+++ UnityUtils/Scripts/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public static class LayerMaskExtensions {
+    /// <summary>
+    /// Checks if the given layer number is contained in the LayerMask.
+    /// </summary>
+    /// <param name="mask">The LayerMask to check.</param>
+    /// <param name="layerNumber">The layer number to check if it is contained in the LayerMask.</param>
+    /// <returns>True if the layer number is contained in the LayerMask, otherwise false.</returns>
+    public static bool Contains(this LayerMask mask, int layerNumber) {
+        return mask == (mask | (1 << layerNumber));
+    }
+}
