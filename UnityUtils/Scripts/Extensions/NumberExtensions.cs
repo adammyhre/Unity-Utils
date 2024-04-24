c71fecf3036c6c232525982cd18bb310806677a0
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-04-23 19:50:04 -0700

    Update NumberExtensions.cs

--- UnityUtils/Scripts/Extensions/NumberExtensions.cs
+++ UnityUtils/Scripts/Extensions/NumberExtensions.cs
@@ -10,6 +10,10 @@ namespace UnityUtils {
             return (float) part / whole;
         }
 
+        public static bool Approx(this float f1, float f2) => Mathf.Approximately(f1, f2);
+        public static bool IsOdd(this int i) => i % 2 == 1;
+        public static bool IsEven(this int i) => i % 2 == 0;
+
         public static int AtLeast(this int value, int min) => Mathf.Max(value, min);
         public static int AtMost(this int value, int max) => Mathf.Min(value, max);
 
