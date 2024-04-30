3fc9c49edf7efacd20ccfee6475eed1fe61201ed
adammyhre <38876398+adammyhre@users.noreply.github.com>
2024-04-30 08:20:22 -0700

    Update ListExtensions.cs

--- UnityUtils/Scripts/Extensions/ListExtensions.cs
+++ UnityUtils/Scripts/Extensions/ListExtensions.cs
@@ -43,13 +43,14 @@ namespace UnityUtils {
         }
 
         /// <summary>
-        /// Shuffles the elements in the list using the Fisher-Yates algorithm.
-        /// This method modifies the input list in-place.
-        /// http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+        /// Shuffles the elements in the list using the Durstenfeld implementation of the Fisher-Yates algorithm.
+        /// This method modifies the input list in-place, ensuring each permutation is equally likely, and returns the list for method chaining.
+        /// Reference: http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
         /// </summary>
         /// <param name="list">The list to be shuffled.</param>
         /// <typeparam name="T">The type of the elements in the list.</typeparam>
-        public static void Shuffle<T>(this IList<T> list) {
+        /// <returns>The shuffled list.</returns>
+        public static IList<T> Shuffle<T>(this IList<T> list) {
             if (rng == null) rng = new Random();
             int count = list.Count;
             while (count > 1) {
@@ -57,6 +58,7 @@ namespace UnityUtils {
                 int index = rng.Next(count + 1);
                 (list[index], list[count]) = (list[count], list[index]);
             }
-        }        
+            return list;
+        }
     }
 }
