03a1859b102869b388d010884569f336e32f4cb0
Adam Myhre <adam@undergalaxie.com>
2024-06-02 04:28:29 -0700

    Add RandomPointInAnnulus with uniform distribution

--- UnityUtils/Scripts/Extensions/Vector2Extensions.cs
+++ UnityUtils/Scripts/Extensions/Vector2Extensions.cs
@@ -26,5 +26,27 @@ namespace UnityUtils {
         public static bool InRangeOf(this Vector2 current, Vector2 target, float range) {
             return (current - target).sqrMagnitude <= range * range;
         }
+        
+        /// <summary>
+        /// Computes a random point in an annulus (a ring-shaped area) based on minimum and 
+        /// maximum radius values around a central Vector2 point (origin).
+        /// </summary>
+        /// <param name="origin">The center Vector2 point of the annulus.</param>
+        /// <param name="minRadius">Minimum radius of the annulus.</param>
+        /// <param name="maxRadius">Maximum radius of the annulus.</param>
+        /// <returns>A random Vector2 point within the specified annulus.</returns>
+        public static Vector2 RandomPointInAnnulus(this Vector2 origin, float minRadius, float maxRadius) {
+            float angle = Random.value * Mathf.PI * 2f;
+            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    
+            // Squaring and then square-rooting radii to ensure uniform distribution within the annulus
+            float minRadiusSquared = minRadius * minRadius;
+            float maxRadiusSquared = maxRadius * maxRadius;
+            float distance = Mathf.Sqrt(Random.value * (maxRadiusSquared - minRadiusSquared) + minRadiusSquared);
+    
+            // Calculate the position vector
+            Vector2 position = direction * distance;
+            return origin + position;
+        }
     }
 }
--- UnityUtils/Scripts/Extensions/Vector3Extensions.cs
+++ UnityUtils/Scripts/Extensions/Vector3Extensions.cs
@@ -49,6 +49,37 @@ namespace UnityUtils {
                 v1.x != 0 ? v0.x / v1.x : v0.x, 
                 v1.y != 0 ? v0.y / v1.y : v0.y, 
                 v1.z != 0 ? v0.z / v1.z : v0.z);  
-        }        
+        }
+        
+        /// <summary>
+        /// Converts a Vector2 to a Vector3 with a y value of 0.
+        /// </summary>
+        /// <param name="v2">The Vector2 to convert.</param>
+        /// <returns>A Vector3 with the x and z values of the Vector2 and a y value of 0.</returns>
+        public static Vector3 ToVector3(this Vector2 v2) {
+            return new Vector3(v2.x, 0, v2.y);
+        }
+        
+        /// <summary>
+        /// Computes a random point in an annulus (a ring-shaped area) based on minimum and 
+        /// maximum radius values around a central Vector3 point (origin).
+        /// </summary>
+        /// <param name="origin">The center Vector3 point of the annulus.</param>
+        /// <param name="minRadius">Minimum radius of the annulus.</param>
+        /// <param name="maxRadius">Maximum radius of the annulus.</param>
+        /// <returns>A random Vector3 point within the specified annulus.</returns>
+        public static Vector3 RandomPointInAnnulus(this Vector3 origin, float minRadius, float maxRadius) {
+            float angle = Random.value * Mathf.PI * 2f;
+            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    
+            // Squaring and then square-rooting radii to ensure uniform distribution within the annulus
+            float minRadiusSquared = minRadius * minRadius;
+            float maxRadiusSquared = maxRadius * maxRadius;
+            float distance = Mathf.Sqrt(Random.value * (maxRadiusSquared - minRadiusSquared) + minRadiusSquared);
+    
+            // Converting the 2D direction vector to a 3D position vector
+            Vector3 position = new Vector3(direction.x, 0, direction.y) * distance;
+            return origin + position;
+        }
     }
 }
