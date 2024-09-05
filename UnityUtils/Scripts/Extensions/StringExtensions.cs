4ad9876904cd6213fc66c6dd4f82e3e9747e7d76
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-09-04 21:31:35 -0700

    Added ConvertToAlphanumeric (#12)
    * Added ConvertToAlphanumeric
    
    This helps maintain consistency and prevents potential issues with invalid characters in string names.
    
    * Started ColorExtensions Class.
    
    Just put in some basic wrappers for the operators, and added a couple simple methods.

--- UnityUtils/Scripts/Extensions/StringExtensions.cs
+++ UnityUtils/Scripts/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UnityUtils {
     public static class StringExtensions {
@@ -42,6 +44,42 @@ namespace UnityUtils {
             }
 
             return val.Substring(startIndex, endIndex - startIndex);
+        }
+
+        /// <summary>
+        /// Converts the input string to an alphanumeric string, optionally allowing periods.
+        /// </summary>
+        /// <param name="input">The input string to be converted.</param>
+        /// <param name="allowPeriods">A boolean flag indicating whether periods should be allowed in the output string.</param>
+        /// <returns>
+        /// A new string containing only alphanumeric characters, underscores, and optionally periods.
+        /// If the input string is null or empty, an empty string is returned.
+        /// </returns>
+        public static string ConvertToAlphanumeric(this string input, bool allowPeriods = false) {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            List<char> filteredChars = new List<char>();
+            int lastValidIndex = -1;
+
+            // Iterate over the input string, filtering and determining valid start/end indices
+            foreach (char character in input
+                         .Where(character => char
+                             .IsLetterOrDigit(character) || character == '_' || (allowPeriods && character == '.'))
+                         .Where(character => filteredChars.Count != 0 || (!char.IsDigit(character) && character != '.'))) {
+
+                filteredChars.Add(character);
+                lastValidIndex = filteredChars.Count - 1; // Update lastValidIndex for valid characters
+            }
+
+            // Remove trailing periods
+            while (lastValidIndex >= 0 && filteredChars[lastValidIndex] == '.') {
+                lastValidIndex--;
+            }
+    
+            // Return the filtered string
+            return lastValidIndex >= 0
+                ? new string(filteredChars.ToArray(), 0, lastValidIndex + 1) : string.Empty;
         }
     }
 }
